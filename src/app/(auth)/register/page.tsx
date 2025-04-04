1dc7654c01898baa06224fc3c04d140fa70f7103
joanpurbaa <joanpurba562@gmail.com>
2025-04-05 03:56:04 +0700

    feat : setup the folder structure

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+export default function register() {
+	return (
+		<>
+			<h1>this is register page</h1>
+		</>
+	);
+}
---
+++ src/app/profil/page.tsx
@@ -0,0 +1,7 @@
+export default function Profil(){
+  return (
+    <>
+      <h1>this is user profil page</h1>
+    </>
+  )
+}
