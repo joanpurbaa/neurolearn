324dfdb3b98cb797e3e1ff39bd1cb2b1e3e30c22
joanpurbaa <joanpurba562@gmail.com>
2025-04-06 21:12:22 +0700

    feat : add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,27 +3,29 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
 const PlusJakartaSans = Plus_Jakarta_Sans({
-  weight: ["200", "300", "400", "500", "600", "700", "800"],
-  subsets: ["latin"],
+	weight: ["200", "300", "400", "500", "600", "700", "800"],
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "NeuroLearn",
-  description: "Gak cuma nonton, tapi juga paham! AI bantu ringkas, kamu tinggal gas quiznya!",
+	title: "NeuroLearn",
+	description:
+		"Gak cuma nonton, tapi juga paham! AI bantu ringkas, kamu tinggal gas quiznya!",
+	icons: {
+		icon: "/favicon.ico",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${PlusJakartaSans.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${PlusJakartaSans.className} antialiased`}>
+				{children}
+			</body>
+		</html>
+	);
 }
